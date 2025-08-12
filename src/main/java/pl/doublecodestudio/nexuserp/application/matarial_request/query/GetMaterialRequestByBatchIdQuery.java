e63162abda3f7562ef7e2e18b2fd16a27ba797e1
Tomasz Dyda <tdyda@tablefoods.com>
2025-08-12 18:14:52 +0200

    feat(material-request): get by batchId endpoint and handler

--- src/main/java/pl/doublecodestudio/nexuserp/application/matarial_request/query/GetMaterialRequestByBatchIdQuery.java
+++ src/main/java/pl/doublecodestudio/nexuserp/application/matarial_request/query/GetMaterialRequestByBatchIdQuery.java
@@ -1,4 +1,4 @@
 package pl.doublecodestudio.nexuserp.application.matarial_request.query;
 
-public record GetMaterialRequestByBatchIdQuery() {
+public record GetMaterialRequestByBatchIdQuery(String batchId) {
 }
