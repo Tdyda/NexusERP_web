67e21b64e604014cee85845b374ef828ef20dbae
Tomasz Dyda <tdyda@tablefoods.com>
2025-08-27 15:31:01 +0200

    Add getAllSubstitutes method to SubstituteService

--- src/main/java/pl/doublecodestudio/nexuserp/application/substitute/service/SubstituteService.java
+++ src/main/java/pl/doublecodestudio/nexuserp/application/substitute/service/SubstituteService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 import pl.doublecodestudio.nexuserp.domain.substitute.entity.Substitute;
 import pl.doublecodestudio.nexuserp.domain.substitute.port.SubstituteRepository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +19,15 @@ public class SubstituteService {
         return repo.findByBaseMaterialId(baseMaterialId).stream()
                 .map(item -> Substitute.create(item.getBaseIndex(), item.getSubstituteIndex()))
                 .toList();
+    }
+
+    public Map<String, List<String>> getAllSubstitutes(Collection<String> baseMaterialIds) {
+        List<Substitute> substitutes = repo.findByBaseMaterialIdIn(baseMaterialIds);
+
+        return substitutes.stream()
+                .collect(Collectors.groupingBy(
+                        Substitute::getBaseIndex,
+                        Collectors.mapping(Substitute::getSubstituteIndex, Collectors.toList())
+                ));
     }
 }
