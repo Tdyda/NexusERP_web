4237f2e4a668c7882764a48371912ac80a8aa129
Tomasz Dyda <tdyda@tablefoods.com>
2025-08-27 15:30:57 +0200

    Add bulk findByBaseMaterialIdIn method to substitute repositories

--- src/main/java/pl/doublecodestudio/nexuserp/domain/substitute/port/SubstituteRepository.java
+++ src/main/java/pl/doublecodestudio/nexuserp/domain/substitute/port/SubstituteRepository.java
@@ -2,8 +2,11 @@ package pl.doublecodestudio.nexuserp.domain.substitute.port;
 
 import pl.doublecodestudio.nexuserp.domain.substitute.entity.Substitute;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface SubstituteRepository {
     List<Substitute> findByBaseMaterialId(String baseMaterialId);
+    List<Substitute> findByBaseMaterialIdIn(Collection<String> baseIndexes);
 }
--- src/main/java/pl/doublecodestudio/nexuserp/infrastructure/phmes/substitute/JpaSubstituteRepository.java
+++ src/main/java/pl/doublecodestudio/nexuserp/infrastructure/phmes/substitute/JpaSubstituteRepository.java
@@ -2,8 +2,10 @@ package pl.doublecodestudio.nexuserp.infrastructure.phmes.substitute;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface JpaSubstituteRepository extends JpaRepository<XlnSubstitutesTomek, XlnSubstitutesTomekId> {
     List<XlnSubstitutesTomek> findByBaseMaterialId(String baseMaterialId);
+    List<XlnSubstitutesTomek> findByBaseMaterialIdIn(Collection<String> baseIndexes);
 }
